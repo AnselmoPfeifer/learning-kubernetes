25dc98a4d142587b8125ed5aa6cdc365da7d93dd
Anselmo <anspfeifer@gmail.com>
2017-11-17 17:27:25 -0300

    Add os arquivos .yaml

--- init.sh
+++ init.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-NAMESPACE='javaneiros'
 
-kubectl create namespace ${NAMESPACE}
+NS='javaneiros'
+kubectl create namespace ${NS}
+
+NS='--namespace javaneiros'
+echo "Instalando o Nginx"
+kubectl apply --filename=k8s/nginx/nginx_configmap.yaml ${NS}
+kubectl apply --filename=k8s/nginx/nginx_service.yaml ${NS}
+kubectl apply --filename=k8s/nginx/nginx_deployment.yaml ${NS}
+
+echo "Instalando o Tomcat"
+kubectl apply --filename=k8s/tomcat/tomcat_configmapusers.yaml ${NS}
+kubectl apply --filename=k8s/tomcat/tomcat_service.yaml ${NS}
+kubectl apply --filename=k8s/tomcat/tomcat_deployment.yaml ${NS}
